0430f0d275bb2f74470cb9bbdd36dfe192cdc1f2
Diana Sutlief <dsutlief@axon.com>
2021-08-30 22:00:03 -0700

    updating lib versions

--- app/src/main/java/org/indiv/dls/games/verboscruzados/model/AnswerPresentation.kt
+++ app/src/main/java/org/indiv/dls/games/verboscruzados/model/AnswerPresentation.kt
@@ -1,7 +1,7 @@
 package org.indiv.dls.games.verboscruzados.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class AnswerPresentation(val isAcross: Boolean,
